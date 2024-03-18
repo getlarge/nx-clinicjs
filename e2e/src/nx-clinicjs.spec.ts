95bff7a97ce4a24561ac30a6996f866075f14bec
getlarge <ed@getlarge.eu>
2024-03-18 17:35:11 +0100

    test: init basic e2e test suite

--- e2e/src/nx-clinicjs.spec.ts
+++ e2e/src/nx-clinicjs.spec.ts
@@ -1,12 +1,16 @@
-import { execSync } from 'child_process';
-import { join, dirname } from 'path';
-import { mkdirSync, rmSync } from 'fs';
+import { execSync } from 'node:child_process';
+import { join, dirname } from 'node:path';
+import { mkdirSync, rmSync, writeFileSync } from 'node:fs';
+import { ProjectConfiguration, readJsonFile } from '@nx/devkit';
 
 describe('nx-clinicjs', () => {
   let projectDirectory: string;
+  let testApp: string;
 
   beforeAll(() => {
-    projectDirectory = createTestProject();
+    const projectConfig = createTestProject();
+    testApp = projectConfig.testApp;
+    projectDirectory = projectConfig.projectDirectory;
 
     // The plugin has been built and published to a local registry in the jest globalSetup
     // Install the plugin built with the latest source code into the test repo
@@ -15,14 +19,17 @@ describe('nx-clinicjs', () => {
       stdio: 'inherit',
       env: process.env,
     });
+
+    addPlugin(projectDirectory);
   });
 
   afterAll(() => {
     // Cleanup the test project
-    rmSync(projectDirectory, {
-      recursive: true,
-      force: true,
-    });
+    projectDirectory &&
+      rmSync(projectDirectory, {
+        recursive: true,
+        force: true,
+      });
   });
 
   it('should be installed', () => {
@@ -32,6 +39,21 @@ describe('nx-clinicjs', () => {
       stdio: 'inherit',
     });
   });
+
+  it('should include the bubbleprof target in the project graph', () => {
+    const result = execSync(`npx nx show project ${testApp} --json`, {
+      cwd: projectDirectory,
+      env: process.env,
+      encoding: 'utf-8',
+    });
+    const project: ProjectConfiguration = JSON.parse(result);
+    expect(Object.keys(project.targets)).toContain('bubbleprof');
+    expect(project.targets.bubbleprof.executor).toBe(
+      '@getlarge/nx-clinicjs:bubbleprof'
+    );
+  });
+
+  it.todo('should be able to run bubbleprof task');
 });
 
 /**
@@ -41,6 +63,7 @@ describe('nx-clinicjs', () => {
 function createTestProject() {
   const projectName = 'test-project';
   const projectDirectory = join(process.cwd(), 'tmp', projectName);
+  const testApp = 'test-app';
 
   // Ensure projectDirectory is empty
   rmSync(projectDirectory, {
@@ -61,5 +84,38 @@ function createTestProject() {
   );
   console.log(`Created test project in "${projectDirectory}"`);
 
-  return projectDirectory;
+  // create app
+  execSync(
+    `npx nx g @nx/node:application ${testApp} --directory apps --as-provided --no-interactive`,
+    {
+      cwd: projectDirectory,
+      stdio: 'inherit',
+      env: process.env,
+    }
+  );
+  // add empty clinicjs config
+  writeFileSync(
+    join(projectDirectory, 'apps', testApp, 'clinic.json'),
+    JSON.stringify({})
+  );
+
+  return { projectDirectory, testApp };
+}
+
+function addPlugin(projectDirectory: string) {
+  // add the plugin to the nx.json file
+  const nxJsonPath = join(projectDirectory, 'nx.json');
+  const nxJson = readJsonFile(nxJsonPath);
+  nxJson.plugins = nxJson.plugins || [];
+  nxJson.plugins.push({
+    plugin: '@getlarge/nx-clinicjs',
+    options: {
+      buildTarget: 'build',
+      doctorTargetName: 'doctor',
+      flameTargetName: 'flame',
+      bubbleprofTargetName: 'bubbleprof',
+      heapTargetName: 'heap',
+    },
+  });
+  writeFileSync(nxJsonPath, JSON.stringify(nxJson, null, 2));
 }
